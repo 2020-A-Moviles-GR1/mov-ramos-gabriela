48464c6814fe89f231d0dd786f11c32674ae593d
Gabriela Ramos <50031675+GabyRamos@users.noreply.github.com>
2020-06-15 07:46:41 -0500

    Clase 4

--- 00-Kotlin/src/Main.kt
+++ 00-Kotlin/src/Main.kt
@@ -1,3 +1,65 @@
-fun main(args:Array<String>){
+import java.util.*
+
+fun main(args: Array<String>) {
     print("Hola")
+    // Ejemplo java:
+    // Int edad = 31;
+    // Mutables
+    var edadProfesor = 31   // No especificamos el tipo de dato
+    // ; No es necesario
+    // Duck Typing
+    // var edadCachorro; X -> necesitamos el tipo de datos
+    var edadCachorro: Int
+    edadCachorro = 3
+    edadProfesor = 32
+    edadCachorro = 4
+    // Inmutables
+    val numeroCuenta = 123456 // NO SE PUEDEN REASIGNAR
+    // numeroCuenta = 123
+
+    // Tipos variables
+    val nombreProfesor: String = "Vicente Adrian"
+    val sueldo: Double = 12.20
+    val apellidosProfesor: Char = 'a'
+    val fechaNacimiento = Date() // new Date()
+
+    if (sueldo == 12.20) {
+
+    } else {
+
+    }
+
+    when (sueldo) {
+        12.20 -> println("Sueldo normal")
+        -12.20 -> println("Sueldo negativo")
+        else -> println("No se reconoce el sueldo")
+    }
+
+    val esSueldoMayorAlEstablecido = if (sueldo == 12.20) true else false
+    // EXPRESION ? X : Y
+    // calcularSueldo(1000.00, 14.00)
+    calcularSueldo(1000.00, 14.00)
+    calcularSueldo(
+            tasa = 16.00,
+            sueldo = 800.00
+    ) // Named Parameters
+    calcularSueldo(700.00)
+    calcularSueldo(sueldo = 650.00)
+}
+
+fun calcularSueldo(
+        sueldo: Double, // Requeridos!
+        tasa: Double = 12.00, // Tiene valor defecto
+        calculoEspecial: Int? = null // Pueden ser nulos
+): Double {
+    if (calculoEspecial != null) {
+        return sueldo * tasa * calculoEspecial
+    } else {
+        return sueldo * tasa
+    }
+}
+
+
+fun imprimirMensaje() { // Unit = Void
+    println("")
 }
