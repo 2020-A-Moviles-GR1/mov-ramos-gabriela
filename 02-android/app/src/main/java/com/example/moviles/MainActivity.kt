3ffe8b19eab80295d71f17e8c6275e0a3d5ee26d
Gabriela Ramos <gabriela.ramos@epn.edu.ec>
2020-07-07 21:45:24 -0500

    Clase 10 - sumar ciclo de vida

--- 02-android/app/src/main/java/com/example/moviles/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/moviles/MainActivity.kt
@@ -1,50 +1,34 @@
 package com.example.moviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_ciclo_vida
+            .setOnClickListener { boton ->
+                // this.irCicloDeVida()
+                irCicloDeVida()
+            }
 
-        Log.i("Activity", "OnCreate")
+
+       // Log.i("Activity", "OnCreate")
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i("Activity", "OnStart")
-
+    fun irCicloDeVida() {
+        val intentExplicito = Intent( //solo necesitamos escribir el nombre de la clase para instanciar un nuevo objecto
+            this,                       //mandamos el this
+           CicloVida::class.java     //mandamos la actividad a donde queremos que se vaya
+        )
+        // this.startActivity(intentExplicito)
+        startActivity(intentExplicito) //para ejecutar necesitamos este metodo que esta dentro de la clase
     }
 
-    override fun onResume() {
-        super.onResume()
 
-        Log.i("Activity", "onResume")
-
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("Activity", "onRestart")
-
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("Activity", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("Activity", "onStop")
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("Activity", "onDestroy")
-    }
 
     }
