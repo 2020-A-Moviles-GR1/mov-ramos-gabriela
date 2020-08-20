c548107287765836fbc6ff0de77e1544d148b234
Gabriela Ramos <gabriela.ramos@epn.edu.ec>
2020-08-19 21:12:23 -0500

    Pokemon Restfull

--- 03-backend/mascotas/api/controllers/UsuarioController.js
+++ 03-backend/mascotas/api/controllers/UsuarioController.js
@@ -10,3 +10,60 @@ module.exports = {
 
 };
 
+//  Restfull
+//  HTTP
+
+// URL METODO PARAMETROS
+
+// PARAMETROS DE RUTA
+
+// http://localhost:1337/usuario/1
+// http://localhost:1337/usuario/2
+// http://localhost:1337/usuario/4/casa/1
+// http://localhost:1337/usuario/4/casa/2
+// http://localhost:1337/usuario/4/casa/5
+
+// PARAMETROS DE CONSULTA
+
+// http://localhost:1337/usuario?nombre=Adrian&apellido=Eguez&edad=20
+// nombre -> Adrian
+// apellido -> Eguez
+// edad -> 20
+
+// PARAMETROS DE CUERPO (EXCEPTO EN GET)
+
+// GET
+// PUT
+// DELETE
+// POST
+
+// ESTANDAR RestFul
+
+// 1- Buscar todos
+// 2- Buscar uno
+// 3- Crear uno
+// 4- Editar uno
+// 5- Eliminar uno
+
+// Usuario
+
+// BUSCAR TODOS
+
+// GET http://localhost:1337/Usuario
+
+// BUSCAR UNO
+
+// GET http://localhost:1337/Usuario/1
+
+// CREAR UNO
+
+// POST http://localhost:1337/Usuario (Parametros de Cuerpo)
+
+// EDITAR UNO
+
+// PUT http://localhost:1337/Usuario/6 (Parametros de Cuerpo)
+
+// ELIMINAR UNO
+
+// DELETE http://localhost:1337/Usuario/3
+
--- 03-backend/mascotas/api/models/Usuario.js
+++ 03-backend/mascotas/api/models/Usuario.js
@@ -5,25 +5,41 @@
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
 
+
 module.exports = {
-
+  tableName: 'epn_usuario',
   attributes: {
-
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+    cedula:{ // nombre atributo
+      type: 'string',
+      required: true, // Por defecto es false
+      columnName: 'epn_cedula',
+      unique: true, // Por defecto es false
+      minLength: 10,
+      maxLength: 25
+    },
+    nombre:{
+      type: 'string',
+      minLength: 3,
+      required: true, // Por defecto es false
+    },
+    correo:{
+      type: 'string',
+      isEmail: true // Por defecto es false
+    },
+    estadoCivil:{
+      type: 'string',
+      isIn: ['Soltero', 'Casado', 'Divorciado', 'Viudo', 'Unión libre'], // Solo estos valores
+      defaultsTo: 'Soltero' // Valor por defecto
+    },
+    password:{
+      type: 'string',
+      regex:  /^[a-zA-Z]\w{3,14}$/
+    },
+ //
   },
-
 };
+
+// BDD
+// APP
+// Movil
 
